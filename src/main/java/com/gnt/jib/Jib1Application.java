97fa70726e1ab11c2cc0d58acfdb1266db18a835
girish <girish@girish-Inspiron-5558>
2021-05-22 17:42:13 +0530

    JavaDoc added

--- src/main/java/com/gnt/jib/Jib1Application.java
+++ src/main/java/com/gnt/jib/Jib1Application.java
@@ -2,7 +2,11 @@ package com.gnt.jib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author girish
+ *
+ */
 @SpringBootApplication
 public class Jib1Application {
 
